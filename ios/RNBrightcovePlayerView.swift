19b36db6cc7993a1f1aacf9c18cd34620e42b395
Dillon Hafer <dillon.hafer@gmail.com>
2022-11-11 13:21:02 -0500

    Properly calculate hex colors

--- ios/RNBrightcovePlayerView.swift
+++ ios/RNBrightcovePlayerView.swift
@@ -1,33 +1,50 @@
 import ExpoModulesCore
 import BrightcovePlayerSDK
 
+func hexStringToUIColor (_ hex:String) -> UIColor {
+		var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+		if (cString.hasPrefix("#")) {
+				cString.remove(at: cString.startIndex)
+		}
+
+		if ((cString.count) != 6) {
+				return UIColor.gray
+		}
+
+		var rgbValue:UInt64 = 0
+		Scanner(string: cString).scanHexInt64(&rgbValue)
+
+		return UIColor(
+				red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+				green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+				blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+				alpha: CGFloat(1.0)
+		)
+}
+
 extension UIColor {
 	public convenience init?(hex: String) {
-		let r, g, b, a: CGFloat
-		print(hex)
-		
-		if hex.hasPrefix("#") {
-			let start = hex.index(hex.startIndex, offsetBy: 1)
-			let hexColor = String(hex[start...])
-			print(hexColor)
-			
-			if hexColor.count >= 6 {
-				let scanner = Scanner(string: hexColor)
-				var hexNumber: UInt64 = 0
-				
-				if scanner.scanHexInt64(&hexNumber) {
-					r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-					g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-					b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-					a = hexColor.count == 8 ? CGFloat(hexNumber & 0x000000ff) / 255 : 1
-					
-					self.init(red: r, green: g, blue: b, alpha: a)
-					return
-				}
-			}
+		var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+		if (cString.hasPrefix("#")) {
+				cString.remove(at: cString.startIndex)
 		}
-		
-		return nil
+
+		if ((cString.count) != 6) {
+				return nil
+		}
+
+		var rgbValue:UInt64 = 0
+		Scanner(string: cString).scanHexInt64(&rgbValue)
+
+		self.init(
+				red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+				green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+				blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+				alpha: CGFloat(1.0)
+		)
+		return;
 	}
 }
 
